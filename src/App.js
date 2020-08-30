09fad1a9c085d16146ec6c29a2bbe12a03d06152
chethanagopinath <chethugopi12@gmail.com>
2020-08-29 23:50:40 -0400

    Started with content of page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import "./style.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div class="main-div">
+      <div class="sub-div">
+        <h1 id='name-header'>Hello, I'm Chethana</h1>
+        <p>I am a graduate student pursuing a masters degree in Computer Science from SUNY Binghamton.</p>
+      </div>
     </div>
   );
 }
