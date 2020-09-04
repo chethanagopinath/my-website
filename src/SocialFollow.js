07449c5925a4f4a82833b11785f01079b9fb141d
chethanagopinath <chethugopi12@gmail.com>
2020-09-04 18:35:11 -0400

    Changed the theme and fonts on the webpage

--- src/SocialFollow.js
+++ src/SocialFollow.js
@@ -6,16 +6,16 @@ import {
   faDev,
   faLinkedin,
   faGithub,
-  faStackoverflow,
-  faWordpress
+  faWordpressSimple
 } from "@fortawesome/free-brands-svg-icons";
 
 export default function SocialFollow() {
   return (
     <div class="social-container">
-      <a href="https://dev.to/chethanagopinath"
-      className="dev social">
-        <FontAwesomeIcon icon={faDev} size="2x" />
+
+      <a href="https://github.com/chethanagopinath/"
+      className="github social">
+        <FontAwesomeIcon icon={faGithub} size="2x" />
       </a>
 
       <a href="https://www.linkedin.com/in/chethana-gopinath/"
@@ -23,19 +23,14 @@ export default function SocialFollow() {
         <FontAwesomeIcon icon={faLinkedin} size="2x" />
       </a>
 
-      <a href="https://github.com/chethanagopinath/"
-      className="github social">
-        <FontAwesomeIcon icon={faGithub} size="2x" />
-      </a>
-
-      <a href="https://stackoverflow.com/users/4403096/chethana-gopinath"
-      className="stackoverflow social">
-        <FontAwesomeIcon icon={faStackoverflow} size="2x" />
+      <a href="https://dev.to/chethanagopinath"
+      className="dev social">
+        <FontAwesomeIcon icon={faDev} size="2x" />
       </a>
 
       <a href="https://apennyeveryweek.wordpress.com/"
       className="wordpress social">
-        <FontAwesomeIcon icon={faWordpress} size="2x" />
+        <FontAwesomeIcon icon={faWordpressSimple} size="2x" />
       </a>
 
 
