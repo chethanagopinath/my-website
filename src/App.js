f682962d326f2cdd2e3649e29575f60345b1851f
chethanagopinath <chethugopi12@gmail.com>
2020-09-04 13:25:56 -0400

    Added icons for social media

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import SocialFollow from "./SocialFollow"
 
 export default function App() {
   return (
-    <div class="main-div">
-      <div class="sub-div">
+    <div classname="main-div">
+      <div classname="sub-div">
         <h1 id='name-header'>Hello, I'm Chethana</h1>
         <p>I am a graduate student pursuing a masters degree in Computer Science from SUNY Binghamton.</p>
+        <SocialFollow />
       </div>
     </div>
   );
--- src/SocialFollow.js
+++ src/SocialFollow.js
@@ -1,9 +1,44 @@
 import React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  faDev,
+  faLinkedin,
+  faGithub,
+  faStackoverflow,
+  faWordpress
+} from "@fortawesome/free-brands-svg-icons";
+
 export default function SocialFollow() {
   return (
     <div class="social-container">
-      <h3>Social Follow</h3>
+      <a href="https://dev.to/chethanagopinath"
+      className="dev social">
+        <FontAwesomeIcon icon={faDev} size="2x" />
+      </a>
+
+      <a href="https://www.linkedin.com/in/chethana-gopinath/"
+      className="linkedin social">
+        <FontAwesomeIcon icon={faLinkedin} size="2x" />
+      </a>
+
+      <a href="https://github.com/chethanagopinath/"
+      className="github social">
+        <FontAwesomeIcon icon={faGithub} size="2x" />
+      </a>
+
+      <a href="https://stackoverflow.com/users/4403096/chethana-gopinath"
+      className="stackoverflow social">
+        <FontAwesomeIcon icon={faStackoverflow} size="2x" />
+      </a>
+
+      <a href="https://apennyeveryweek.wordpress.com/"
+      className="wordpress social">
+        <FontAwesomeIcon icon={faWordpress} size="2x" />
+      </a>
+
+
     </div>
   );
 }
