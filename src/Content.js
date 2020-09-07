529ec83c33a87be1921c00cc3ebc301a1ad20566
chethanagopinath <chethugopi12@gmail.com>
2020-09-07 18:57:52 -0400

    Added Resume button and styling to page

--- src/Content.js
+++ src/Content.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function Content(){
   return(
       <div className="sub-div">
-        <h1>Hello, I'm Chethana </h1>
+        <h1>Hello, I'm Chethana 🌈✨</h1>
+        <div id="resume-div">
+          <a id="resume-link" href = "https://drive.google.com/drive/folders/1Iu9BAvvmFJpdzwZa8i-kkv3J58JJPBYk?usp=sharing" download> Resume </a>
+        </div>
         <p>
         I am a CS grad student at SUNY Binghamton, an aspiring software engineer, a problem-solver and a baker. My skills include Ruby, Rails, Python, Java, Javascript, HTML, CSS, MySQL and Git. I have had industry experience in automated testing using Selenium, a bit of Java and Cucumber at Wolters Kluwer ELM Solutions where I worked with clients like Walmart, JPMorgan Chase, Nissan and NCR.
         </p>
@@ -11,10 +14,10 @@ export default function Content(){
         Being a budding open-source contributor, I spend my weekends working on beginner-friendly issues in shf-project from AgileVentures, ifme project and other repositories on GitHub which allow me to build up the confidence to contribute more (waiting eagerly for HacktoberFest!) 
         </p>
         <p>
-        I am a member of <a href = "https://linktr.ee/breakingbarriers">Breaking Barriers in Tech</a>, a very nurturing and inclusive community which aims to combat obstacles faced by minorities in tech. We are building a strong unified community through informative workshops, fun events and such. I am also a registered mentor at the mentorship program at <a href = "https://www.lotusstemm.org/">LotusSTEMM</a>, an organization that is working to provide a networking and leadership platform for South Asian women in Science, Technology, Engineering, Math and Medicine. 
+        I am a member of <a className = "links" href = "https://linktr.ee/breakingbarriers">Breaking Barriers in Tech</a>, a very nurturing and inclusive community which aims to combat obstacles faced by minorities in tech. We are building a strong unified community through informative workshops, fun events and such. I am also a registered mentor at the mentorship program at <a className = "links" href = "https://www.lotusstemm.org/">LotusSTEMM</a>, an organization that is working to provide a networking and leadership platform for South Asian women in Science, Technology, Engineering, Math and Medicine. 
         </p>
         <p>
-        During my free time, I work on cool side projects to solve problems that I face every day. Currently, I am working on building a Chrome extension called TinyNotes, which is a basic note-taking app that would stay on my new tab window built using Javascript, HTML and CSS. I have also recently joined as a volunteer at <a href = "https://www.womenwhocode.com/python">Women Who Code Python track</a> to help with slack channel moderation and event co-ordination. I try to add value to young, aspiring software engineers such as myself by writing out tiny technical posts at <a href="https://dev.to/">dev.to</a>, which is an incredibly talented community that I came across some time ago.
+        During my free time, I work on cool side projects to solve problems that I face every day. Currently, I am working on building a Chrome extension called TinyNotes, which is a basic note-taking app that would stay on my new tab window built using Javascript, HTML and CSS. I have also recently joined as a volunteer at <a className = "links" href = "https://www.womenwhocode.com/python">Women Who Code Python track</a> to help with slack channel moderation and event co-ordination. I try to add value to young, aspiring software engineers such as myself by writing out tiny technical posts at <a className = "links" href="https://dev.to/">dev.to</a>, which is an incredibly talented community that I came across some time ago.
         </p>
         <p>
         Please do check out my socials below if you are interested in knowing more about some of my work and reaching out to me. Thank you for visiting my website! Take care and stay healthy!
